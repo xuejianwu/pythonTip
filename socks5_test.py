c38ef539d1b2d30d172896e1ca2b8052e342721c
Xuejianwu <32357432+xuejianwu@users.noreply.github.com>
2022-04-22 15:20:02 +0800

    Create socks5_test.py

---
+++ socks5_test.py
@@ -0,0 +1,5 @@
+import requests, socket, socks
+socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 10800)
+socket.socket = socks.socksocket
+
+requests.get('https://www.google.com', timeout=5).status_code
